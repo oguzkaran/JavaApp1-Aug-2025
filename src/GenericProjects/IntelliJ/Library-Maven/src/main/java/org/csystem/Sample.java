7dbfc5409c8cfc39f4847859a35921fd48f1f392
Oğuz Karan <oguzkaran@csystem.org>
2025-08-12 21:31:51 +0300

    update

--- src/GenericProjects/IntelliJ/Library-Maven/src/main/java/org/csystem/Sample.java
+++ src/GenericProjects/IntelliJ/Library-Maven/src/main/java/org/csystem/Sample.java
@@ -1,13 +1,3 @@
-/*-------------------------------------------------------------
-	FILE		: Sample.java
-	AUTHOR		: JavaApp2-Jan-2024 Group
-	Last UPDATE	: 09th Aug 2024
-
-	Sample Class
-
-	Copyleft (c) 1993 C and System Programmers Association
-	All Right Free
--------------------------------------------------------------*/
 package org.csystem;
 
 public class Sample {
