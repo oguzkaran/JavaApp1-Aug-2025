aa07fa63a0804c5410e7b56512dfa90329c957ba
Oğuz Karan <oguzkaran@csystem.org>
2025-08-14 20:54:16 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,8 +1,18 @@
 package org.csystem.app;
 
+
+import com.karandev.io.util.console.Console;
+import org.csystem.util.string.StringUtil;
+
+import java.util.Random;
+
 class Application {
     public static void run(String[] args)
     {
+        Random random = new Random();
+        int n = Console.readInt("Input count:", "Invalid value!...");
 
+        while (n-- > 0)
+            Console.writeLine("%s", StringUtil.generateRandomTextTR(random, random.nextInt(5, 14)));
     }
 }
