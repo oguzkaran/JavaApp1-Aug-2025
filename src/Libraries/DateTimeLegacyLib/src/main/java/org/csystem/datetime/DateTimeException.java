2d32fb74e1823f23b27e83b84e60b8c4be49c8a1
Oğuz Karan <oguzkaran@csystem.org>
2025-12-25 20:54:42 +0300

    Deprecated

--- src/Libraries/DateTimeLegacyLib/src/main/java/org/csystem/datetime/DateTimeException.java
+++ src/Libraries/DateTimeLegacyLib/src/main/java/org/csystem/datetime/DateTimeException.java
@@ -1,6 +1,6 @@
 package org.csystem.datetime;
 
-@Deprecated(forRemoval = true, since = "8")
+@Deprecated(forRemoval = true, since = "2.0.0")
 public class DateTimeException extends RuntimeException  {
     public DateTimeException()
     {}
--- src/Libraries/DateTimeLegacyLib/src/main/java/org/csystem/datetime/Month.java
+++ src/Libraries/DateTimeLegacyLib/src/main/java/org/csystem/datetime/Month.java
@@ -1,6 +1,6 @@
 package org.csystem.datetime;
 
-@Deprecated(forRemoval = true, since = "8")
+@Deprecated(forRemoval = true, since = "2.0.0")
 public enum Month {
     JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
 }
