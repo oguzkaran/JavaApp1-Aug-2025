38c410d2a950fc1cbac9b13d3968d4b2cf95189f
Oğuz Karan <oguzkaran@csystem.org>
2026-01-06 22:18:18 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,11 +1,23 @@
 package org.csystem.app;
 
+import com.karandev.io.util.console.Console;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class Application {
     public static void run(String[] args)
     {
+        while (true) {
+            try {
+                var clsName = Console.read("Input class name:");
+                if ("exit".equals(clsName))
+                    break;
+                var cls = Class.forName(clsName);
 
+                log.info("Name: {}", cls.getName());
+            } catch (ClassNotFoundException e) {
+                log.error("Class not found:{}", e.getMessage());
+            }
+        }
     }
 }
