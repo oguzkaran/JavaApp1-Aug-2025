c9f988d198f4a4e4960b17c9e5e043ed63a75876
Oğuz Karan <oguzkaran@csystem.org>
2026-02-24 21:37:45 +0300

    add IntRange class with unary operator

--- src/Libraries/GeneratorLib/src/main/java/org/csystem/generator/range/IntRange.java
+++ src/Libraries/GeneratorLib/src/main/java/org/csystem/generator/range/IntRange.java
@@ -5,17 +5,23 @@ import lombok.experimental.Accessors;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.function.IntUnaryOperator;
 
 @AllArgsConstructor
 @Accessors(prefix = "m_")
 public class IntRange implements Iterable<Integer> {
     private final int m_begin;
     private final int m_end;
-    private final int m_step;
+    private final IntUnaryOperator m_intUnaryOperator;
 
     public IntRange(int begin, int end)
     {
         this(begin, end, 1);
+    }
+
+    public IntRange(int begin, int end, int step)
+    {
+        this(begin, end, a -> a + step);
     }
 
     @Override
@@ -37,7 +43,7 @@ public class IntRange implements Iterable<Integer> {
                     throw new NoSuchElementException("Can not generate a value");
 
                 var result = value;
-                value += m_step;
+                value = m_intUnaryOperator.applyAsInt(value);
 
                 return result;
             }
--- src/Libraries/GeneratorLib/src/test/java/org/csystem/generator/range/IntRangeTest.java
+++ src/Libraries/GeneratorLib/src/test/java/org/csystem/generator/range/IntRangeTest.java
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test;
 public class IntRangeTest {
     private int m_value;
 
+    private void step2ForEachCallback(int val, int step)
+    {
+        Assertions.assertEquals(m_value, val);
+        m_value += step;
+    }
+
+    private void operatorForEachCallback(int val)
+    {
+        Assertions.assertEquals(m_value, val);
+        m_value = 2 * m_value + 1;
+    }
+
     @Test
     public void givenStep_whenOne_thenGenerateValues()
     {
@@ -16,20 +28,24 @@ public class IntRangeTest {
         new IntRange(begin, end).forEach(val -> Assertions.assertEquals(m_value++, val));
     }
 
-    private void step2ForEachCallback(int val, int step)
-    {
-        Assertions.assertEquals(m_value, val);
-        m_value += step;
-    }
-
     @Test
     public void givenStep_whenTwo_thenGenerateValues()
     {
         var begin = 10;
         var end = 20;
         var step = 2;
-        m_value = begin ;
+        m_value = begin;
 
         new IntRange(begin, end, step).forEach(val -> step2ForEachCallback(val, step));
+    }
+
+    @Test
+    public void givenOperator_whenTwoTimesPlusOne_thenGenerateValues()
+    {
+        var begin = 10;
+        var end = 500;
+        m_value = begin;
+
+        new IntRange(begin, end, a -> 2 * a + 1).forEach(this::operatorForEachCallback);
     }
 }
