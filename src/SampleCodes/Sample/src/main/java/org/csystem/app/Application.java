3046313bab55edb6397d3c62e416da89d42fa0a0
Oğuz Karan <oguzkaran@csystem.org>
2026-04-17 13:40:45 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,30 +3,29 @@ package org.csystem.app;
 import com.karandev.io.util.console.Console;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.util.LinkedList;
 
 @Slf4j
 class Application {
     public static void run(String[] args)
     {
-        Console.writeLine(Util.CountMinutes("9:00am-10:00am")); //60
-        Console.writeLine(Util.CountMinutes("1:00pm-11:00am")); // 1320
-        Console.writeLine(Util.CountMinutes("12:30pm-12:00am")); // 690
-        Console.writeLine(Util.CountMinutes("1:23am-1:08am")); // 1425
-    }
-}
+        var linkedList = new LinkedList<String>();
 
-class Util {
-    public static long CountMinutes(String str)
-    {
-        var formatter = DateTimeFormatter.ofPattern("h:mma");
-        var info = str.split("-");
-        var startTime = LocalTime.parse(info[0].toUpperCase(), formatter);
-        var endTime = LocalTime.parse(info[1].toUpperCase(), formatter);
+        while (true) {
+            var s = Console.read("Input text:");
 
-        return startTime.isBefore(endTime) ? ChronoUnit.MINUTES.between(startTime, endTime)
-                : 24 * 60 - ChronoUnit.MINUTES.between(endTime,startTime);
+            if ("quit".equals(s))
+                break;
+
+            linkedList.push(s);
+        }
+
+        while (!linkedList.isEmpty()) {
+            var str = linkedList.pop();
+
+            Console.write("%s ", str);
+        }
+
+        Console.writeLine();
     }
 }
