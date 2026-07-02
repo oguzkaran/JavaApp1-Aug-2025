b0feefda2ee09ae3baa3faf27b9ddbd5b4acc748
Oğuz Karan <oguzkaran@csystem.org>
2026-07-02 21:03:59 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -2,23 +2,25 @@ package org.csystem.app;
 
 import com.karandev.io.util.console.Console;
 import lombok.extern.slf4j.Slf4j;
-import org.csystem.util.array.ArrayUtil;
-import org.csystem.util.numeric.NumberUtil;
+import org.csystem.util.datasource.factory.ProductFactory;
 
-import java.util.Random;
-import java.util.stream.IntStream;
+import java.math.BigDecimal;
 
 @Slf4j
 class Application {
     public static void run(String[] args)
     {
-        var r = new Random();
+        try {
+            var threshold = new BigDecimal(Console.readDouble("Threshold:"));
 
-        int [] a = IntStream.generate(() -> r.nextInt(1, 100))
-                .filter(n -> {Console.writeLine("%d generated", n); return NumberUtil.isPrime(n);})
-                .limit(10).toArray();
-
-        ArrayUtil.print(a);
-        Console.writeLine();
+            ProductFactory.loadFromTextFile(args[0]).get().PRODUCTS
+                    .stream()
+                    .filter(p -> p.getStock() > 0)
+                    .filter(p -> p.getCost().compareTo(threshold) > 0)
+                    .forEach(Console::writeLine);
+        }
+        catch (Exception e) {
+            Console.Error.writeLine("Error while loading product file:%s", e.getMessage());
+        }
     }
 }
