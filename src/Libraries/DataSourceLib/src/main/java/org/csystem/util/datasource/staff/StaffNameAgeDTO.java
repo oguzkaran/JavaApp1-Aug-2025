332ed069bc1f68cf4880be0966968b595cb636d4
Oğuz Karan <oguzkaran@csystem.org>
2026-07-16 20:09:57 +0300

    update

--- src/Libraries/DataSourceLib/src/main/java/org/csystem/util/datasource/staff/StaffNameAgeDTO.java
+++ src/Libraries/DataSourceLib/src/main/java/org/csystem/util/datasource/staff/StaffNameAgeDTO.java
@@ -1,28 +1,13 @@
 package org.csystem.util.datasource.staff;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Data;
 
+@Builder
+@Data
 public final class StaffNameAgeDTO {
     public final String name;
     public final double age;
-
-    public StaffNameAgeDTO(String name, double age)
-    {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(name, age);
-    }
-
-    @Override
-    public boolean equals(Object other)
-    {
-        return other instanceof StaffNameAgeDTO s && s.name.equals(name) && s.age == age;
-    }
 
     @Override
     public String toString()
--- src/Libraries/DataSourceLib/src/main/java/org/csystem/util/datasource/staff/StaffNameRestDayDTO.java
+++ src/Libraries/DataSourceLib/src/main/java/org/csystem/util/datasource/staff/StaffNameRestDayDTO.java
@@ -1,29 +1,16 @@
 package org.csystem.util.datasource.staff;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.DayOfWeek;
 import java.util.Objects;
 
+@Builder
+@Data
 public final class StaffNameRestDayDTO {
     public final String name;
     public final DayOfWeek restDay;
-
-    public StaffNameRestDayDTO(String name, DayOfWeek restDay)
-    {
-        this.name = name;
-        this.restDay = restDay;
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(name, restDay);
-    }
-
-    @Override
-    public boolean equals(Object other)
-    {
-        return other instanceof StaffNameRestDayDTO s && s.name.equals(name) && s.restDay == restDay;
-    }
 
     @Override
     public String toString()
