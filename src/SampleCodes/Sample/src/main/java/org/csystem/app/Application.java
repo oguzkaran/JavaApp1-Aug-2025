e4015f2271ca996ab2b8215a616cd514d8a9072e
Oğuz Karan <oguzkaran@csystem.org>
2026-09-01 20:58:18 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -2,17 +2,37 @@ package org.csystem.app;
 
 import com.karandev.io.util.console.Console;
 import lombok.extern.slf4j.Slf4j;
+import org.csystem.util.numeric.NumberUtil;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.LongStream;
+
+import static com.karandev.io.util.console.CommandLineArgs.checkLengthEquals;
+
 
 @Slf4j
 class Application {
-    private static int ms_val;
-
     public static void run(String[] args)
     {
-        var random = new Random();
+        try {
+            checkLengthEquals(args.length, 1, "Wrong number of arguments");
+            int count = Short.parseShort(args[0]);
 
-        random.ints(1, 50).distinct().limit(6).sorted().forEach(v -> Console.write("%d ", v));
+            if (count <= 0)
+                throw new NumberFormatException();
+
+            var random = new Random();
+
+            var primes = LongStream.generate(random::nextLong)
+                    .peek(v -> log.info("{}", v))
+                    .filter(NumberUtil::isPrime).limit(count).toArray();
+
+            Console.writeLine("Generated prime numbers:");
+            Arrays.stream(primes).forEach(Console::writeLine);
+        }
+        catch (NumberFormatException ignore) {
+            Console.Error.writeLine("Invalid count value");
+        }
     }
 }
--- src/SampleCodes/Sample/src/main/java/org/csystem/game/lottery/NumericLottery.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/game/lottery/NumericLottery.java
@@ -1,7 +1,7 @@
 package org.csystem.game.lottery;
 
-import java.util.TreeSet;
 import java.util.random.RandomGenerator;
+import java.util.stream.Stream;
 
 public class NumericLottery {
     private final RandomGenerator m_randomGenerator;
@@ -11,30 +11,9 @@ public class NumericLottery {
         m_randomGenerator = randomGenerator;
     }
 
-    public int [] getNumbers()
-    {
-        var numbers = new int[6];
-
-        var treeSet = new TreeSet<Integer>();
-
-        while (treeSet.size() != 6)
-            treeSet.add(m_randomGenerator.nextInt(1, 50));
-
-        int i = 0;
-
-        for (var val : treeSet)
-            numbers[i++] = val;
-
-        return numbers;
-    }
-
     public int [][] getNumbers(int n)
     {
-        var columns = new int[n][];
-
-        for (var i = 0; i < n; ++i)
-            columns[i] = getNumbers();
-
-        return columns;
+        return Stream.generate(() -> m_randomGenerator.ints(1, 50).distinct().limit(6).sorted().toArray())
+                .limit(n).toArray(int[][]::new);
     }
 }
